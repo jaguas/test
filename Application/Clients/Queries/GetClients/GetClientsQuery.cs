e5c6302a34b17c8dfab538903f2b7bcd96cfe909
jaguas <joao.aguas@gmail.com>
2021-01-05 11:55:51 +0000

    Update GetClientsQuery.cs

--- Application/Clients/Queries/GetClients/GetClientsQuery.cs
+++ Application/Clients/Queries/GetClients/GetClientsQuery.cs
@@ -3,17 +3,18 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Application.Clients.Queries.GetClients
 {
-    public class GetClientsQuery : IRequest<GetClientsVm>
+    public class GetClientsQuery : IRequest<List<ClientDto>>
     {
     }
 
-    public class GetClientsQueryHandler : IRequestHandler<GetClientsQuery, GetClientsVm>
+    public class GetClientsQueryHandler : IRequestHandler<GetClientsQuery, List<ClientDto>>
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
@@ -24,15 +25,13 @@ namespace Application.Clients.Queries.GetClients
             _mapper = mapper;
         }
 
-        public async Task<GetClientsVm> Handle(GetClientsQuery request, CancellationToken cancellationToken)
+        public async Task<List<ClientDto>> Handle(GetClientsQuery request, CancellationToken cancellationToken)
         {
-            return new GetClientsVm { 
-                Clients = await _context.Clients
+            return await _context.Clients
                     .AsNoTracking()
                     .ProjectTo<ClientDto>(_mapper.ConfigurationProvider)
                     .OrderBy(c => c.Name)
-                    .ToListAsync(cancellationToken)
-            };
+                    .ToListAsync(cancellationToken);
         }
     }
 }

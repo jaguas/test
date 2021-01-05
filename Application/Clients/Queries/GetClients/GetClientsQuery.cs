44919030984f08b46eabc5ee8a3c9439329e1db1
Joao Aguas <joao.aguas@gmail.com>
2021-01-05 10:57:13 +0000

    Initial commit

---
+++ Application/Clients/Queries/GetClients/GetClientsQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Clients.Queries.GetClients
+{
+    public class GetClientsQuery : IRequest<GetClientsVm>
+    {
+    }
+
+    public class GetClientsQueryHandler : IRequestHandler<GetClientsQuery, GetClientsVm>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetClientsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<GetClientsVm> Handle(GetClientsQuery request, CancellationToken cancellationToken)
+        {
+            return new GetClientsVm { 
+                Clients = await _context.Clients
+                    .AsNoTracking()
+                    .ProjectTo<ClientDto>(_mapper.ConfigurationProvider)
+                    .OrderBy(c => c.Name)
+                    .ToListAsync(cancellationToken)
+            };
+        }
+    }
+}
---
+++ Application/Clients/Queries/GetClients/GetClientsVm.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Application.Clients.Queries.GetClients
+{
+    public class GetClientsVm
+    {
+        public IList<ClientDto> Clients { get; set; }
+    }
+}
